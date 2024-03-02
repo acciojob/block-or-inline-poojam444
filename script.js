ddc7426a2d08763d697164c28de8d35214fea7d8
poojam444 <93283150+poojam444@users.noreply.github.com>
2024-03-02 23:35:05 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-cy.get(`[data-ns-test="${testId}"]`).parent().then(ans => {
-    let text = ans.text();
-    console.log(text); // Add this line
-    text = text.replace('Test1', '').replace('Test2', '').trim();
-    expect(text).to.eq(`Answer: ${expectedAnswer}`);
-});
